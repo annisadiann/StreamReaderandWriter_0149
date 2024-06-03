6d236adbe72ec50757bb14853b6c19f5893dda68
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2024-06-03 10:45:09 +0700

    membuka blok try-catch

--- exeptionstandard/exeptionstandard.cpp
+++ exeptionstandard/exeptionstandard.cpp
@@ -8,5 +8,16 @@ using namespace std;
 int main()
 {
 	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1,2,3 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+	}
 	
 }
